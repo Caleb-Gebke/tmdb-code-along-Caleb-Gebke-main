a6033529d240b6a0dfe418b7e8aa3c5d8d3bd0b4
Caleb Gebke <caleb.gebke@student.swic.edu>
2025-05-11 15:48:00 -0500

    feet render header

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import Header from './components/header'
+
+export default function App() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-900">
+      <Header />
+    </main>
+  )
+}
---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header>
+      <h1 className="bg-gradient-to-r from-blue-500 to-green-500 bg-clip-text text-4xl font-bold text-transparent uppercase">
+        {' '}
+        Movie Database
+      </h1>
+    </header>
+  )
+}
